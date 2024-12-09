3b154cb39137d5aec24162999b0bece32adf02ab
John A. Samuel <johnayobami77@proton.me>
2024-12-09 03:59:02 +0100

    ...

--- src/components/Onboarding.jsx
+++ src/components/Onboarding.jsx
@@ -6,23 +6,23 @@ const Onboarding = () => {
             <div className='relative h-[110vh] w-full flex flex-col items-start justify-end'>
                 <div className='absolute bottom-0 w-full opacity-60 h-2/5 bg-black blur z-10'></div>
                 <img 
-                    src="https://assets.lummi.ai/assets/QmXuxZu9J6Nxum9JTPPNd52SQgdBAm1UknGxJFq9gq4fKN?auto=format&w=1500" 
+                    src="https://st3.depositphotos.com/13152218/15594/i/380/depositphotos_155947526-stock-photo-young-smiling-multiethnic-women-looking.jpg" 
                     className='absolute w-full h-full object-cover'
                     alt="Onboarding illustration" />
-                <div className='absolute w-full left-1/2 -translate-x-1/2'>
+                <div className='absolute w-full left-1/2 -translate-x-1/2 p-6 pb-2'>
                     <div className="flex flex-col space-y-4 items-start text-left text-white">
                         <h1 className="text-2xl font-semibold">Ask Questions with Ease</h1>
                         <p className="text-sm font-light">Post questions in any subject and get clear, reliable answers from students and contributors. Learning starts with a single question.</p>
                     </div>
                 </div>
-            </div>
-            <div className="flex flex-col items-start w-full justify-center space-y-2 p-6">
+            <div className="flex flex-col items-start w-full justify-center space-y-2 p-6 pb-0">
                 <Link to="/register" className="w-full">
                     <button className='w-full bg-primary text-white text-center py-3 px-4 rounded-lg' type="button">
                         Get started
                     </button>
                 </Link>
                 <p className="w-full text-sm light:text-gray-400 dark:text-white">Already have an account? <Link to="/login" className='font-bold underline transition duration-300 ease-in-out'>Log in</Link></p>
+            </div>
             </div>
         </div>
     )
