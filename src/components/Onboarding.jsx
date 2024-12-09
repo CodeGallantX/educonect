a7ba005c60efefa50f0a7ddb29b71977fa3e2755
John A. Samuel <johnayobami77@proton.me>
2024-12-09 04:13:50 +0100

    Updates

--- src/components/Onboarding.jsx
+++ src/components/Onboarding.jsx
@@ -8,7 +8,7 @@ const Onboarding = () => {
                 <img 
                     // src="https://media.istockphoto.com/id/1588289977/photo/happy-black-females-student-during-a-class-in-lecture-hall-looking-at-camera.jpg?s=612x612&w=0&k=20&c=o2l4DsMWHhZ3Oqq8hxrGoKqJfC1S2kAfnjUHlRJuD-k=" 
                     src="https://media.istockphoto.com/id/2160333909/photo/multiracial-group-of-university-students-sitting-in-the-cafeteria-studying-together-after.jpg?s=612x612&w=0&k=20&c=YsttfMlL_cNQg1T37F6O7_sqDjkHR8fEBsx26O4h1iQ=" 
-                    className='absolute w-full h-full object-cover object-left z-0'
+                    className='absolute w-full h-full object-cover z-0'
                     alt="Onboarding illustration" />
                 <div className='absolute w-full bottom-1/4 translate-y-16 left-1/2 -translate-x-1/2 p-6 pb-2 z-20'>
                     <div className="flex flex-col space-y-3 items-start text-left text-white">
